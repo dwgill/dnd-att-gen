9ee708e088376f1b5b518ad591287176f30f5ae6
Daniel Gill <dgill@nexient.com>
2017-10-10 22:46:47 -0400

    Refactor script.ts
    Just keep the script full of att gen logic for now.

--- script.ts
+++ script.ts
@@ -2,98 +2,142 @@
 // <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js"></script>
 // <script>
 
-function d6() {
-    return _.random(1, 6, false);
+// FEATURES:
+// DICE ROLLS:
+// - 3d6
+// - 4d6k3
+// - 2d6+6
+
+// CONSTRAINTS:
+// - Straight
+// - At least 2 scores >= X
+// - At least 1 score >= X
+// - Sum(mods) >= 2
+// - Sum(mods) >= 1
+// - Sum(scores) >= 70
+ 
+interface Score {
+    component_dice: number[];
+    discarded_dice: number[];
 }
 
-/** General representation of an attribute score. */
-abstract class AttributeScore {
-    /** The dice rolls that constitute this score. */
-    components: number[];
-    /** The name of the attribute associated with this score, if any. */
-    name?: String;
+type ScoreSet = Score[];
 
-    constructor(dice_results: number[], name?: String) {
-        this.components = dice_results;
-        this.name = name? name.toUpperCase() : null;
-    }
-
-    /** Computes the modifier for this score. */
-    modifier(): number {
-        return Math.floor((this.value() - 10) / 2);
-    }
-    /** Indicates whether a name is associated with this attribute. */
-    has_name(): boolean {
-        return this.name != null;
-    }
-    
-    /** Computes the score's total value. */
-    abstract value(): number;
-    /** Returns a list of the numbers used in determining the score's value. */
-    abstract utilized_components(): number[];
-    /** Returns a list of the numbers excluded from the scores value. */
-    abstract discarded_components(): number[];
+interface DiceRollMethod {
+    (): Score;
 }
 
-/** Representation of a classic attribute score.
- * i.e. 3d6
- */
-class ClassicScore extends AttributeScore {
-    constructor(dice_results?: number[], name?: String) {
-        if (!dice_results) {
-            super(_.times(3, d6), name);
-        } else {
-            super(dice_results, name);
+interface ScoreSetValidator {
+    (scoreSet: ScoreSet): boolean;
+}
+
+enum Attribute {
+    str = "STR",
+    dex = "DEX",
+    con = "CON",
+    int = "INT",
+    wis = "WIS",
+    cha = "CHA",
+}
+
+function d6() { return _.random(1, 6, false); }
+
+function roll_3d6(): Score {
+    return {
+        component_dice: [d6(), d6(), d6()],
+        discarded_dice: [],
+    };
+}
+
+function roll_4d6k3(): Score {
+    var the_dice = [d6(), d6(), d6(), d6()];
+    var lowest_dice = the_dice.splice(_.min(the_dice), 1);
+    return {
+        component_dice: the_dice,
+        discarded_dice: lowest_dice,
+    };
+}
+
+function roll_2d6p6(): Score {
+    return {
+        component_dice: [6, d6(), d6()],
+        discarded_dice: [],
+    }
+}
+
+function roll_until_valid(diceRollMethod: DiceRollMethod, scoreSetValidator: ScoreSetValidator) {
+    while(true) {
+        var scores = _.times(6, diceRollMethod);
+        if (scoreSetValidator(scores)) {
+            return scores;
         }
     }
-    /** Computes the score's total value. */
-    value() {
-        return _.sum(this.components);
-    }
-    /** Returns a list of the numbers used in determining the score's value. */
-    utilized_components() {
-        return _.clone(this.components);
-    }
-    /** Returns a list of the numbers excluded from the scores value. */
-    discarded_components() {
-        return [];
-    }
 }
 
-/** Representation of a contemporary attribute score.
- * i.e. 4d6d1
- */
-class ModernScore extends AttributeScore {
-    constructor(dice_results?: number[], name?: String) {
-        if (!dice_results) {
-            super(_.times(4, d6), name);
-        } else {
-            super(dice_results, name);
-        }
-    }
-    /** Computes the score's total value. */
-    value() {
-        return _.sum(this.components) - _.min(this.components);
-    }
-    /** Returns a list of the numbers used in determining the score's value. */
-    utilized_components() {
-        var min_index = _.findIndex(this.components, _.min(this.components));
-        return _.filter(this.components, (value, index) => index !== min_index);
-    }
-    /** Returns a list of the numbers excluded from the scores value. */
-    discarded_components() {
-        return [_.min(this.components)];
-    }
+function calc_modifier(score: Score) {
+    return Math.floor((_.sum(score.component_dice) - 10) / 2);
 }
 
-interface AttValidator  {
-    (scores: number[]): boolean;
+function print_score(score: Score, attr?: Attribute): string {
+    var score_modifier: number = calc_modifier(score);
+    var result: string[] = [];
+
+    // Attribute Type (if applicable)
+    if (attr) {
+        result.push(attr, ": ");
+    }
+
+    // The score itself
+    result.push(_.sum(score.component_dice).toString());
+
+    // The modifier
+    result.push(" (");
+    if (score_modifier >= 0) { result.push("+"); }
+    result.push(score_modifier.toString(), ")");
+
+    // The composition of the score
+    result.push(" [");
+    result.push(score.component_dice.toString());
+
+    // The discarded dice (if any)
+    if (score.discarded_dice.length > 0) {
+        result.push("(", score.discarded_dice.toString(), ")");
+    }
+    result.push("]");
+
+    return result.join("");
 }
 
+function at_least_x_scores_of_y_value(scoreSet: ScoreSet, x: number, y: number) {
+    return _(scoreSet)
+        .map(score => score.component_dice)
+        .map(dice => _.sum(dice))
+        .filter(score => score >= x)
+        .size() >= y;
+}
 
+function validate_colville_orig(scoreSet: ScoreSet) {
+    return at_least_x_scores_of_y_value(scoreSet, 15, 2);
+}
 
-function matt_colville_validate(scores: AttributeScore[]) {
-    return _.filter(scores, (score) => score.value() >= 15).length >= 2;
+function validate_colville_lite(scoreSet: ScoreSet) {
+    return at_least_x_scores_of_y_value(scoreSet, 15, 1);
+}
+
+function mod_of_at_least(scoreSet: ScoreSet, value: number) {
+    return _(scoreSet).map(calc_modifier).sum().valueOf() >= value;
+}
+
+function validate_colville_neo(scoreSet: ScoreSet) {
+    return mod_of_at_least(scoreSet, 2);
+}
+
+function validate_all(...validators: ScoreSetValidator[]) {
+    return function(scoreSet: ScoreSet) {
+        return _(validators)
+        .map(validator => validator(scoreSet))
+        .every();
+    }
 }
 
 // </script>
